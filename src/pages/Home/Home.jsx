dafe2b8e855ae3efdff7d9180a0812eb0ecb2a88
toomajbandad <tbandad@gmail.com>
2025-04-08 17:29:03 +0200

    `

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <h1>This is Home page</h1>
+      <h1>This is Home Page</h1>
     </div>
   );
 }
